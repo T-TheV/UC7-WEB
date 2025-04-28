20d3d5832e1719b455a9d4087f0d3b90707278a9
T-theV <djardim322@gmail.com>
2025-04-28 13:56:44 -0300

    feat: Adicionar estrutura inicial do projeto com Vite e React, incluindo componentes e estilos

---
+++ UC7/mobileFirst/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import styles from "./Home.module.css";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Main from "../../components/Main/Main";
+
+function Home() {
+    return (
+        <>
+             <Header />
+            <Main />
+            <Footer />
+           
+        </>
+
+
+    );
+}
+
+export default Home;
