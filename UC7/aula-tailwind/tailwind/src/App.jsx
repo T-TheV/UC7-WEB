f6c0f9c1ab88a6c19e5164be33f64487946d2247
T-theV <djardim322@gmail.com>
2025-05-15 17:17:47 -0300

    feat: Adicionar configuração inicial do projeto com Vite, React e Tailwind CSS

---
+++ UC7/aula-tailwind/tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+
+      <div className="min-h-screen bg-blue-100 flex items-center justify-center ">
+        <div className="p-8 bg-whithe roundend-2x1 shadow-lg">
+          <h1 className="text-2x1 font-bold text-purple-600">Hello World, React + Tailwind</h1>
+          <p className="mt-2 text-gray-600">Tentaremos formatar algo!</p>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
